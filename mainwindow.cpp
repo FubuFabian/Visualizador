5717237ec91434ddb29fec7fb67ace84fe3d9cb3
Fabian Torres <dae.wong@gmail.com>
2016-05-30 15:46:51 -0500

    axialview

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -71,10 +71,6 @@ MainWindow::MainWindow(QWidget *parent) :
 	coronalLayout->setSpacing(0);
 	coronalLayout->QLayout::addWidget(coronalWidget);
 	ui->coronalView->setLayout(coronalLayout);
-
-	resliceAxesSagital = vtkSmartPointer<vtkMatrix4x4>::New();
-	reslicerSagital = vtkSmartPointer<vtkImageReslice>::New();
-	transformSagital = vtkSmartPointer<vtkTransform>::New();
 }
 
 MainWindow::~MainWindow()
@@ -141,23 +137,26 @@ void MainWindow::displayVol()
 
 void MainWindow::setRenderingData()
 {
+	imageStyle = vtkSmartPointer<vtkInteractorStyleImage>::New();
+
 	viewerSagital = vtkSmartPointer<vtkImageViewer2>::New();
 	imageActorSagital = vtkSmartPointer<vtkImageActor>::New();
-	imageStyle = vtkSmartPointer<vtkInteractorStyleImage>::New();
-	
     sagitalWidget->SetRenderWindow(viewerSagital->GetRenderWindow());
 	imageActorSagital = viewerSagital->GetImageActor();
 	imageActorSagital->InterpolateOff();
 	viewerSagital->GetRenderWindow()->GetInteractor()->SetInteractorStyle(imageStyle);
+
+	viewerAxial = vtkSmartPointer<vtkImageViewer2>::New();
+	imageActorAxial = vtkSmartPointer<vtkImageActor>::New();
+    axialWidget->SetRenderWindow(viewerAxial->GetRenderWindow());
+	imageActorAxial = viewerSagital->GetImageActor();
+	imageActorAxial->InterpolateOff();
+	viewerAxial->GetRenderWindow()->GetInteractor()->SetInteractorStyle(imageStyle);
 }
 
 void MainWindow::setSlicesData()
 {
-	ui->rotXSld->setRange(-90,90);
-	ui->rotXSld->setTickInterval(1);
-	ui->rotXSld->setValue(0);
-
-
+	std::cout<<"Displaying Slices"<<std::cout;
 	volumeData->GetSpacing(spacing);
 	volumeData->GetOrigin(origin);
     volumeData->GetDimensions(dimensions);
@@ -166,44 +165,96 @@ void MainWindow::setSlicesData()
 	centerSlice[1] = floor(dimensions[1]*0.5)-1;
 	centerSlice[2] = floor(dimensions[2]*0.5)-1;
 
-	std::cout<<"Displaying Sagittal View"<<std::endl;
-	
-	sliceSagital = centerSlice[0];
-	angleXSagital = 0;
-
 	positionCenter[0] = origin[0] + spacing[0] * centerSlice[0];
 	positionCenter[1] = origin[1] + spacing[1] * centerSlice[1]; 
-    positionCenter[2] = origin[2] + spacing[2] * centerSlice[2]; 
+    positionCenter[2] = origin[2] + spacing[2] * centerSlice[2];
+
+	configSagitalView();
+	configAxialView();
+
+	ui->rotXSld->setRange(-90,90);
+	ui->rotXSld->setTickInterval(1);
+	ui->rotXSld->setValue(0);
+
+	ui->rotYSld->setRange(-90,90);
+	ui->rotYSld->setTickInterval(1);
+	ui->rotYSld->setValue(0);
+
+	ui->rotZSld->setRange(-90,90);
+	ui->rotZSld->setTickInterval(1);
+	ui->rotZSld->setValue(0);
+
+	ui->sliceSld->setRange(0,dimensions[0]-1);
+	ui->sliceSld->setTickInterval(1);
+	ui->sliceSld->setValue(centerSlice[0]);
+
+	displayAxial();
+}
+
+void MainWindow::configSagitalView()
+{
+	resliceAxesSagital = vtkSmartPointer<vtkMatrix4x4>::New();
+	reslicerSagital = vtkSmartPointer<vtkImageReslice>::New();
+	transformSagital = vtkSmartPointer<vtkTransform>::New();
+
+	sliceSagital = centerSlice[0];
+	angleXSagital = 0;
+	angleYSagital = 0;
+	angleZSagital = 0; 
+
+	transformSagital->PostMultiply();
+	transformSagital->Translate(positionCenter[0],origin[1],origin[2]);
 
 	resliceAxesSagital->DeepCopy(sagitalElements);
 
 	reslicerSagital->SetInput(volumeData);
 	reslicerSagital->SetOutputDimensionality(2);
 	reslicerSagital->SetResliceAxes(resliceAxesSagital);
-	reslicerSagital->SetResliceAxesOrigin(positionCenter);
+	reslicerSagital->SetResliceTransform(transformSagital);
 	reslicerSagital->SetInterpolationModeToLinear();
-	reslicerSagital->SetOutputExtent(0,dimensions[0],0,dimensions[2],0,0);
-	//reslicerSagital->SetOutputOrigin(-positionCenter[0],-positionCenter[2],0);
-	reslicerSagital->SetOutputSpacing(spacing);
+}
 
-	transformSagital->PostMultiply();
+void MainWindow::configAxialView()
+{
+	resliceAxesAxial = vtkSmartPointer<vtkMatrix4x4>::New();
+	reslicerAxial = vtkSmartPointer<vtkImageReslice>::New();
+	transformAxial = vtkSmartPointer<vtkTransform>::New();
 
-	if(ui->sagitalViewBtn->isChecked()){
-		ui->sliceSld->setRange(0,dimensions[0]-1);
-		ui->sliceSld->setTickInterval(1);
-		ui->sliceSld->setValue(centerSlice[0]);
-	}
+	sliceAxial = centerSlice[2];
+	angleXAxial = 0;
+	angleYAxial = 0;
+	angleZAxial = 0; 
 
+	transformAxial->PostMultiply();
+	transformAxial->Translate(origin[0],origin[1],positionCenter[2]);
+
+	resliceAxesAxial->DeepCopy(axialElements);
+
+	reslicerAxial->SetInput(volumeData);
+	reslicerAxial->SetOutputDimensionality(2);
+	reslicerAxial->SetResliceAxes(resliceAxesAxial);
+	reslicerAxial->SetResliceTransform(transformAxial);
+	reslicerAxial->SetInterpolationModeToLinear();
 }
 
 void MainWindow::displaySagital()
 {
-	reslicerSagital->SetResliceTransform(transformSagital);
 	reslicerSagital->Update();
 	sliceImageSagital = reslicerSagital->GetOutput();
+	sliceImageSagital->Update();
 
 	viewerSagital->SetInput(sliceImageSagital);
     viewerSagital->Render();
+}
+
+void MainWindow::displayAxial()
+{
+	reslicerAxial->Update();
+	sliceImageAxial = reslicerAxial->GetOutput();
+	sliceImageAxial->Update();
+
+	viewerAxial->SetInput(sliceImageAxial);
+    viewerAxial->Render();
 }
 
 void MainWindow::openMHD()
@@ -231,6 +282,11 @@ void MainWindow::reslice(int slice)
 		transformSagital->Translate(translate*spacing[0],0,0);
 		sliceSagital = slice;
 		displaySagital();
+	}else if(ui->axialViewBtn->isChecked()){
+		double translate = slice - sliceAxial;
+		transformAxial->Translate(0,0,translate*spacing[2]);
+		sliceAxial = slice;
+		displayAxial();
 	}
 }
 
@@ -243,7 +299,90 @@ void MainWindow::rotateX(int angle)
 		transformSagital->Translate(0,(positionCenter[1]-origin[1]),(positionCenter[2]-origin[2]));
 		angleXSagital = angle;
 		displaySagital();
-
+	}else if(ui->axialViewBtn->isChecked()){
+		double rotate = angle - angleXAxial;
+		transformAxial->Translate(-(positionCenter[0]-origin[0]),-(positionCenter[1]-origin[1]),0);
+		transformAxial->RotateX(rotate);
+		transformAxial->Translate((positionCenter[0]-origin[0]),(positionCenter[1]-origin[1]),0);
+		angleXAxial = angle;
+		displayAxial();
 	}
 
+}
+
+void MainWindow::rotateY(int angle)
+{
+	if(ui->sagitalViewBtn->isChecked()){
+		double rotate = angle - angleYSagital;
+		transformSagital->Translate(0,-(positionCenter[1]-origin[1]),-(positionCenter[2]-origin[2]));
+		transformSagital->RotateY(rotate);
+		transformSagital->Translate(0,(positionCenter[1]-origin[1]),(positionCenter[2]-origin[2]));
+		angleYSagital = angle;
+		displaySagital();
+	}else if(ui->axialViewBtn->isChecked()){
+		double rotate = angle - angleYAxial;
+		transformAxial->Translate(-(positionCenter[0]-origin[0]),-(positionCenter[1]-origin[1]),0);
+		transformAxial->RotateY(rotate);
+		transformAxial->Translate((positionCenter[0]-origin[0]),(positionCenter[1]-origin[1]),0);
+		angleYAxial = angle;
+		displayAxial();
+	}
+
+}
+
+void MainWindow::rotateZ(int angle)
+{
+	if(ui->sagitalViewBtn->isChecked()){
+		double rotate = angle - angleZSagital;
+		transformSagital->Translate(0,-(positionCenter[1]-origin[1]),-(positionCenter[2]-origin[2]));
+		transformSagital->RotateZ(rotate);
+		transformSagital->Translate(0,(positionCenter[1]-origin[1]),(positionCenter[2]-origin[2]));
+		angleZSagital = angle;
+		displaySagital();
+	}else if(ui->axialViewBtn->isChecked()){
+		double rotate = angle - angleZAxial;
+		transformAxial->Translate(-(positionCenter[0]-origin[0]),-(positionCenter[1]-origin[1]),0);
+		transformAxial->RotateZ(rotate);
+		transformAxial->Translate((positionCenter[0]-origin[0]),(positionCenter[1]-origin[1]),0);
+		angleZAxial = angle;
+		displayAxial();
+	}
+}
+
+void MainWindow::sagitalBtnClicked(bool value)
+{
+	if(!value){
+		ui->sagitalViewBtn->setChecked(true);
+	}else{
+		ui->sagitalViewBtn->setChecked(true);
+		ui->axialViewBtn->setChecked(false);
+		ui->coronalViewBtn->setChecked(false);
+		ui->volViewBtn->setChecked(false);
+
+		ui->sliceSld->setRange(0,dimensions[0]-1);
+		ui->sliceSld->setValue(sliceSagital);
+
+		ui->rotXSld->setValue(angleXSagital);
+		ui->rotYSld->setValue(angleYSagital);
+		ui->rotZSld->setValue(angleZSagital);
+	}
+}
+
+void MainWindow::axialBtnClicked(bool value)
+{
+	if(!value){
+		ui->axialViewBtn->setChecked(true);
+	}else{
+		ui->sagitalViewBtn->setChecked(false);
+		ui->axialViewBtn->setChecked(true);
+		ui->coronalViewBtn->setChecked(false);
+		ui->volViewBtn->setChecked(false);
+
+		ui->sliceSld->setRange(0,dimensions[2]-1);
+		ui->sliceSld->setValue(sliceAxial);
+
+		ui->rotXSld->setValue(angleXAxial);
+		ui->rotYSld->setValue(angleYAxial);
+		ui->rotZSld->setValue(angleZAxial);
+	}
 }
--- mainwindow.h
+++ mainwindow.h
@@ -60,25 +60,39 @@ private:
 	**/
 	QVTKWidget *coronalWidget;
 
-	/** \brief The filename of the selected volume */
+	/**  
+	The filename of the selected volume
+	**/
 	QString volumeFilename;
 
-	/** The displayed volume data*/
+	/** 
+	The displayed volume data
+	**/
     vtkSmartPointer<vtkVolume> volume;
 
-	/** The displayed volume data*/
+	/** 
+	The displayed volume data
+	**/
     vtkSmartPointer<vtkImageData> volumeData;
 
-	/** The displayed volume opacity*/
+	/** 
+	The displayed volume opacity
+	**/
     vtkSmartPointer<vtkPiecewiseFunction> volumeScalarOpacity;
 
-    /** The displayed volume properties*/
+    /** 
+	The displayed volume properties
+	**/
     vtkSmartPointer<vtkVolumeProperty> volumeProperty;
 
-	/** The VTK render window*/
+	/** 
+	The VTK render window
+	**/
     vtkSmartPointer<vtkRenderWindow> renwin;
 
-    /** The VTK renderer */
+    /** 
+	The VTK renderer 
+	**/
     vtkSmartPointer<vtkRenderer> renderer;
 
 	/**
@@ -96,18 +110,15 @@ private:
 	**/
     int dimensions[3];
 
-	/** \brief The center slices in each view */ 
+	/**
+	The center slices in each view 
+	**/ 
 	double centerSlice[3];
 
 	/**
-	 Position of center
+	Position of center
 	**/
 	double positionCenter[3];
-
-	/**
-	Data for the axial plane
-	**/
-	static const double axialElements[16];
 
 	/**
 	Data for the coronal plane
@@ -145,9 +156,19 @@ private:
 	int sliceSagital;
 
 	/**
-	 Angle of X of sagital slice
+	 Angle X of sagital slice
 	**/
 	int angleXSagital;
+
+	/**
+	 Angle Y of sagital slice
+	**/
+	int angleYSagital;
+
+	/**
+	 Angle Z of sagital slice
+	**/
+	int angleZSagital;
 	
 	/**
 	 sliced sagital image
@@ -160,10 +181,64 @@ private:
 	vtkSmartPointer<vtkImageActor> imageActorSagital;
 
 	/**
+	Data for the axial plane
+	**/
+	static const double axialElements[16];
+
+	/**
+	Matrix containing the  translation and rotation of the slice axial plane
+	**/
+	vtkSmartPointer<vtkMatrix4x4> resliceAxesAxial;
+
+	/**
+	Matrix containing the  translation and rotation of the slice axial plane
+	**/
+	vtkSmartPointer<vtkTransform> transformAxial;
+
+	/**
+	Reslicer that slice the volume and gives the sliced axial image as an output
+	**/
+	vtkSmartPointer<vtkImageReslice> reslicerAxial;
+
+	/**
+	Image Viewer that render the sliced axial image
+	**/
+	vtkSmartPointer<vtkImageViewer2> viewerAxial;
+
+	/**
+	number of axial slice
+	**/
+	int sliceAxial;
+
+	/**
+	Angle X of axial slice
+	**/
+	int angleXAxial;
+
+	/**
+	Angle Y of axial slice
+	**/
+	int angleYAxial;
+
+	/**
+	Angle Z of axial slice
+	**/
+	int angleZAxial;
+	
+	/**
+	sliced axial image
+	**/
+	vtkSmartPointer<vtkImageData> sliceImageAxial;
+	
+	/**
+	ImageActor of the sliced axial image
+	**/
+	vtkSmartPointer<vtkImageActor> imageActorAxial;
+
+	/**
 	Interactor style of the viewer, it has the basic interaction with a 2D image
 	**/
 	vtkSmartPointer<vtkInteractorStyleImage> imageStyle;
-
 
 	/**
 	Display the volume in volWidget
@@ -174,26 +249,69 @@ private:
 	Set the data for slicers
 	**/
 	void setSlicesData();
-
+	
+	/**
+	Set the data for volume rendering
+	**/
 	void setRenderingData();
+
+	/**
+	Configuration of sagital view
+	**/
+	void configSagitalView();
 
 	/**
 	Display the slice sagital image
 	**/
 	void displaySagital();
 
+	/**
+	Configuration of axial view
+	**/
+	void configAxialView();
+
+	/**
+	Display the slice axial image
+	**/
+	void displayAxial();
 
 
 private slots:
 
-	/**
-   * \brief Set a volume filename and open in format MHD
-   */
+    /**
+    Set a volume filename and open in format MHD
+    **/
     void openMHD();
 
+	/**
+    Select sagital View
+    **/
+	void sagitalBtnClicked(bool);
+
+	/**
+    Select axial view
+    **/
+	void axialBtnClicked(bool);
+
+	/**
+    Set slice in selected view
+    **/
 	void reslice(int);
 
+	/**
+    Rotate slice through x in selected view 
+    **/
 	void rotateX(int);
+
+	/**
+    Rotate slice through y in selected view 
+    **/
+	void rotateY(int);
+
+	/**
+    Rotate slice through z in selected view 
+    **/
+	void rotateZ(int);
 };
 
 #endif // MAINWINDOW_H
